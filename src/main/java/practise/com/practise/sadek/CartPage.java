82752dc1e779f6c69afd1afc854db66ad16df0c3
Sadek Ahmed <sadekahmed@sadeks-air.myfiosgateway.com>
2022-10-18 21:43:55 -0400

    Crated Cart page

---
+++ src/main/java/practise/com/practise/sadek/CartPage.java
@@ -0,0 +1,11 @@
+package practise.com.practise.sadek;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CartPage {
+	WebDriver driver;
+	
+	By cartIcon= By.id("cart");
+
+}
